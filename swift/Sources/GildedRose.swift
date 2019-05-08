8a209581a29a8a8837d50c92d819a28289fd8046
Ryan Salton <rsalton@AirPortrHQ037.local>
2019-05-08 22:03:11 +0100

    General syntax clean-up; removing unnecessary nested statements, removing brackets where not needed and updating increment syntax

--- swift/Sources/GildedRose.swift
+++ swift/Sources/GildedRose.swift
@@ -7,7 +7,6 @@ public class GildedRose {
     }
     
     public func updateQuality() {
-        
         let maxQuality = 50
         
         for i in 0..<items.count {
@@ -16,51 +15,39 @@ public class GildedRose {
             let isConcertTicket = item.name == "Backstage passes to a TAFKAL80ETC concert"
             let isSulfuras = item.name == "Sulfuras, Hand of Ragnaros"
             
-            if (!isAgedBrie && !isConcertTicket) {
-                if (item.quality > 0) {
-                    if (!isSulfuras) {
-                        item.quality = item.quality - 1
-                    }
+            if !isAgedBrie && !isConcertTicket {
+                if item.quality > 0 && !isSulfuras {
+                    item.quality -= 1
                 }
             } else {
-                if (item.quality < maxQuality) {
-                    item.quality = item.quality + 1
+                if item.quality < maxQuality {
+                    item.quality += 1
                     
-                    if (isConcertTicket) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
+                    if isConcertTicket {
+                        if item.sellIn < 11 && item.quality < 50 {
+                            item.quality += 1
                         }
                         
-                        if (item.sellIn < 6) {
-                            if (item.quality < maxQuality) {
-                                item.quality = item.quality + 1
-                            }
+                        if item.sellIn < 6 && item.quality < maxQuality {
+                            item.quality += 1
                         }
                     }
                 }
             }
             
-            if (!isSulfuras) {
-                item.sellIn = item.sellIn - 1
+            if !isSulfuras {
+                item.sellIn -= 1
             }
             
-            if (item.sellIn < 0) {
-                if (!isAgedBrie) {
-                    if (!isConcertTicket) {
-                        if (item.quality > 0) {
-                            if (!isSulfuras) {
-                                item.quality = item.quality - 1
-                            }
-                        }
+            if item.sellIn < 0 {
+                if !isAgedBrie {
+                    if !isConcertTicket && (item.quality > 0 && !isSulfuras) {
+                        item.quality -= 1
                     } else {
-                        item.quality = item.quality - item.quality
+                        item.quality -= item.quality
                     }
-                } else {
-                    if (item.quality < maxQuality) {
-                        item.quality = item.quality + 1
-                    }
+                } else if item.quality < maxQuality {
+                    item.quality += 1
                 }
             }
         }
