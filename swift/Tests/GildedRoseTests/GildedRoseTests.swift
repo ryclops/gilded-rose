6cd765d989930f59794f2adfb2041c83ae1d4c82
Ryan Salton <rsalton@AirPortrHQ037.local>
2019-05-08 18:56:37 +0100

    Added test for expired items degrading twice as fast

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -11,6 +11,22 @@ class GildedRoseTests: XCTestCase {
         app.updateQuality();
         XCTAssertEqual("foo", app.items[0].name);
     }
+    
+    func testQualityDegradesTwiceAsFastAfterSellByDate() {
+        let items = [
+            Item(name: "NotExpiredItem", sellIn: 5, quality: 5),
+            Item(name: "ExpiredItem", sellIn: -1, quality: 5)
+        ]
+        
+        let unexpiredStartQuality = items[0].quality
+        let expiredStartQuality = items[1].quality
+        
+        let app = GildedRose(items: items);
+        app.updateQuality()
+        
+        XCTAssertEqual(unexpiredStartQuality - 1, app.items[0].quality)
+        XCTAssertEqual(expiredStartQuality - 2, app.items[1].quality)
+    }
 }
 
 #if os(Linux)
