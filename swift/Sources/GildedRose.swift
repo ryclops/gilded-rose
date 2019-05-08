9a38945a6baa92da5dde4a05e2368776f49215e1
Ryan Salton <rsalton@AirPortrHQ037.local>
2019-05-08 21:52:37 +0100

    Added local variables to store references to item rather than string and array searching each time. Cleaner and better performance

--- swift/Sources/GildedRose.swift
+++ swift/Sources/GildedRose.swift
@@ -9,50 +9,55 @@ public class GildedRose {
     public func updateQuality() {
         
         for i in 0..<items.count {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+            let item = items[i]
+            let isAgedBrie = item.name == "Aged Brie"
+            let isConcertTicket = item.name == "Backstage passes to a TAFKAL80ETC concert"
+            let isSulfuras = item.name == "Sulfuras, Hand of Ragnaros"
+            
+            if (!isAgedBrie && !isConcertTicket) {
+                if (item.quality > 0) {
+                    if (!isSulfuras) {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
                     
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (isConcertTicket) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
                         
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1
                             }
                         }
                     }
                 }
             }
             
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (!isSulfuras) {
+                item.sellIn = item.sellIn - 1
             }
             
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (!isAgedBrie) {
+                    if (!isConcertTicket) {
+                        if (item.quality > 0) {
+                            if (!isSulfuras) {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = item.quality - item.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
                     }
                 }
             }
